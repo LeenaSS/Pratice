a7ecf1e4effea65cb296ad36b4970180d5e7d932
LeenaSS <148950622+LeenaSS@users.noreply.github.com>
2023-10-26 21:27:03 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphisum
+{
+    public class Parentclass
+    {
+        public Parentclass()
+        {
+            Console.WriteLine(" parent class calles");
+        }
+        public Parentclass(string name) 
+        {
+            Console.WriteLine(name);
+        }
+    }
+    public class Childclass : Parentclass
+    {
+        public Childclass(): base(" Name is raju")
+        {
+            Console.WriteLine(" child class called");
+        }
+    }
+    public class Con
+    {
+        static void Main(string[] args)
+        {
+            Childclass cobj = new Childclass();
+
+        }
+    }
+}
---
+++ Polymorphisum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphisum
+{
+    // polymorphisum enables us  to invoke derived class
+    // methods throungh  a base class reference variable using at run time 
+    public class employee
+    {
+        public int id = 2;
+        public string name = "shilpa";
+        public virtual void details()
+        {
+            Console.WriteLine("Name : "+name+" Id :"+id);
+        }
+    }
+    public class firstemployee: employee
+    {
+
+        public override void details() 
+        {
+            Console.WriteLine("Name : " + name + " Id :" + id+" fulltime employee");
+        }
+    }
+    public class partemployee:employee
+    {
+        public override void details()
+        {
+            Console.WriteLine("Name : " + name + " Id :" + id+" part time employee");
+        }
+    }
+    public  class program
+    {
+        static void Main(string[] args)
+        {
+            employee[] employee = new employee[3];
+            employee[0] = new employee();
+            employee[1] = new firstemployee();
+            employee[2] = new partemployee();
+            foreach(var item in employee) 
+            {
+                item.details();
+            }
+
+        }
+    }
+}
